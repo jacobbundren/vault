dceb07acdef2161465d2f417774a18c01e5c74ff
Jacob Bundren <jacob.bundren@tpginc.com>
2023-03-31 17:38:45 -0500

    fixed modal toggling need to add drag n drop and upload service

---
+++ src/components/Upload.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="uploadModalVisible"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn
+          icon
+          color="secondary"
+          @click="showUploadModal()"
+          v-bind="props"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+
+      <v-card color="background">
+        <v-card-text>
+          Drag files or click the + icon to upload files.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn variant="tonal" color="secondary" @click="uploadModalVisible = false">Upload</v-btn>
+          <v-btn variant="tonal" color="error" @click="uploadModalVisible = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, Ref} from "vue";
+
+let uploadModalVisible: Ref<boolean> = ref(false);
+function showUploadModal(): void {
+  uploadModalVisible.value = true;
+}
+</script>
+
+<style scoped>
+  .upload__modal {
+
+  }
+</style>
--- src/views/DocumentGrid.vue
+++ src/views/DocumentGrid.vue
@@ -1,36 +1,30 @@
 <template>
-    <v-row >
-      <v-col
-        v-for="n in numDocs"
-        :key="n"
-        class="d-flex child-flex"
-        cols="1"
-      >
-       <GridItem :file="{
-         id: n,
-         filename: `Image ${n}`,
-         size: 123,
-         type: FileType.JPG,
-         url: `https://picsum.photos/500/300?image=${n}`
-       }" />
-      </v-col>
-
-    </v-row>
-    <v-btn
-      icon
-      color="secondary"
-      class="documentGrid__addFileButton"
-      @click="showUploadModal()"
+  <v-row >
+    <v-col
+      v-for="n in numDocs"
+      :key="n"
+      class="d-flex child-flex"
+      cols="1"
     >
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
+     <GridItem :file="{
+       id: n,
+       filename: `Image ${n}`,
+       size: 123,
+       type: FileType.JPG,
+       url: `https://picsum.photos/500/300?image=${n}`
+     }" />
+    </v-col>
+  </v-row>
+  <Upload class="documentGrid__addFileButton"/>
 </template>
 <script setup lang="ts">
 import GridItem from "../components/GridItem.vue";
+import Upload from "../components/Upload.vue";
 import {FileType} from "@/types/FileType";
-import { ref } from "vue";
+import {Ref, ref} from "vue";
 
-const numDocs = ref(32)
+let numDocs: Ref<number> = ref(32)
+
 
 </script>
 
